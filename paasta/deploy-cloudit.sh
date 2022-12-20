5728eb3ca3c639878d4973eee2a23b03559ba47a
asiaa <asiaa@innogrid.com>
2022-12-20 13:54:24 +0900

    feat: Cloudit V5.0 - bosh deployment 작성

--- paasta/deploy-cloudit.sh
+++ paasta/deploy-cloudit.sh
@@ -3,11 +3,11 @@
 BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
 
 bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
-	-o operations/openstack.yml \
+	-o operations/cloudit.yml \
 	-o operations/use-haproxy.yml \
 	-o operations/use-haproxy-public-network.yml \
 	-o operations/use-postgres.yml \
-	-o operations/cce-postgres.yml \
+	-o operations/cce.yml \
 	-o operations/rename-network-and-deployment.yml \
 	-l vars.yml \
-	-l ../../common/common_vars.yml
+	-l /root/workspace/paasta-5.5.2/deployment/common/common_vars_latest.yml > deploy.manifes
